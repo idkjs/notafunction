ca75aad631eb64408818feb2fdae4f33c8e8c3b6
Alain Armand <aarmand.inbox@gmail.com>
2018-04-06 10:25:18 +0200

    updated MyBanner.js to new syntax, works. See readmen

--- src/MyBanner.js
+++ src/MyBanner.js
@@ -1,13 +1,25 @@
 var ReactDOM = require("react-dom");
 var React = require("react");
-var App = React.createClass({
-  displayName: "MyBanner",
-  render: function() {
-    if (this.props.show) {
-      return React.createElement("div", null, this.props.message);
-    } else {
-      return null;
-    }
+// var App = React.createClass({
+//   displayName: "MyBanner",
+//   render: function() {
+//     if (this.props.show) {
+//       return React.createElement("div", null, this.props.message);
+//     } else {
+//       return null;
+//     }
+//   }
+// });
+var App = function(props) {
+  if (props.show) {
+    return React.createElement(
+      "div",
+      null,
+      "Here's the message from the owner: " + props.message
+    );
+  } else {
+    return null;
   }
-});
+};
+App.displayName = "MyBanner";
 module.exports = App;
